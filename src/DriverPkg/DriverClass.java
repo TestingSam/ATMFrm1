44afef7130dfa04ad05579aacd2501bd33b53d3b
samir <samiralurhp@gmail.com>
2020-08-23 17:59:33 +0530

    Reflections Concept Completed

--- src/DriverPkg/DriverClass.java
+++ src/DriverPkg/DriverClass.java
@@ -1,9 +1,13 @@
 package DriverPkg;
 import java.io.File;
+import java.util.*;
 
 import java.io.FileInputStream;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
@@ -16,7 +20,11 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class DriverClass {
-
+	public static List<String> StepNo= new ArrayList<>();
+	public static List<String> Description= new ArrayList<>();
+	public static List<String> Keyword= new ArrayList<>();
+	public static List<String> ObjectIdentifier= new ArrayList<>();
+	public static List<String> TestData= new ArrayList<>();
     public void readExcel(String filePath,String fileName,String sheetName) throws IOException{
     File file =    new File(filePath+"\\"+fileName);
     FileInputStream inputStream = new FileInputStream(file);
@@ -30,19 +38,45 @@ public class DriverClass {
     }
     Sheet Sheet = Workbook.getSheet(sheetName);
     int rowCount = Sheet.getLastRowNum()-Sheet.getFirstRowNum();
-    for (int i = 0; i < rowCount+1; i++) {
+    for (int i = 1; i < rowCount+1; i++) {
         Row row = Sheet.getRow(i);
         for (int j = 0; j < row.getLastCellNum(); j++) {
-            System.out.print(row.getCell(j).getStringCellValue()+"|| ");
+            //System.out.print(row.getCell(j).getStringCellValue()+"|| ");
+        	switch(j) {
+        	case 0:
+        		StepNo.add(row.getCell(j).getStringCellValue());
+        		break;
+        	case 1:
+        		Description.add(row.getCell(j).getStringCellValue());
+        		break;
+        	case 2:
+        		Keyword.add(row.getCell(j).getStringCellValue());
+        		break;
+        	case 3:
+        		ObjectIdentifier.add(row.getCell(j).getStringCellValue());
+        		break;
+        	case 4:
+        		TestData.add(row.getCell(j).getStringCellValue());
+        		break;
+        	}
         }
-        System.out.println();
     } 
     }  
 
-    public static void main(String...strings) throws IOException{
+    public static void main(String...strings) throws IOException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
 	    DriverClass objExcelFile = new DriverClass();
+	    KeywordClass keywordClass= new KeywordClass();
+	    Class cls = keywordClass.getClass();
+	    Method[] methods = cls.getMethods();
 	    String filePath = System.getProperty("user.dir")+"\\src\\Resources";
 	    objExcelFile.readExcel(filePath,"TestCase.xlsx","Sheet1");
-    }
+	    Iterator<String> itr= DriverClass.Keyword.iterator();
+	    while(itr.hasNext()) {
+	    	String currentKeyword=itr.next();
+	    	System.out.println(currentKeyword);
+	    	Method methodcall1 = cls.getDeclaredMethod(currentKeyword.toString().trim(), int.class); 
+			methodcall1.invoke(keywordClass, 19); 
+	    	}
+	    }
 
 }
---
+++ src/DriverPkg/KeywordClass.java
@@ -0,0 +1,15 @@
+package DriverPkg;
+
+public class KeywordClass {
+	public void CLICK(int j) {
+		System.out.println("Inside CLICK"+ j);
+	}
+	
+	public void SENDKEYS(int j) {
+		System.out.println("Inside SENDKEYS"+ j);
+	}
+	
+	public void TAPPING(int j) {
+		System.out.println("Inside TAPPING"+ j);
+	}
+}
